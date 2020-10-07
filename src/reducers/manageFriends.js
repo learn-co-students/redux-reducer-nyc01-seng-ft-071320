036480384b9fd057c0e70bf1d99057f664ef13eb
Jake Mills <jake.mills.05@gmail.com>
2020-10-07 18:22:44 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return { friends: [...state.friends, action.friend] };
+    case "REMOVE_FRIEND":
+      const newFriendsList = [...state.friends];
+      const removedFriend = state.friends.findIndex(
+        (friend) => friend.id === action.id
+      );
+      newFriendsList.splice(removedFriend, 1);
+      return { friends: newFriendsList };
+    default:
+      return state;
+  }
 }
